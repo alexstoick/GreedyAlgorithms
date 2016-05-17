92f0aaef24cf425a50d267010d7cdfc9a240b797
Daniel Grant <danieljohngrant@gmail.com>
2016-05-17 18:32:30 +0100

    WIP solution

--- src/algo.js
+++ src/algo.js
@@ -10,6 +10,39 @@
  */
 
 function coinChange (amount, coins) {
+  var iterations = 0;
+  var selectedCoins = [];
+
+  // sort coins high to low (high is best)
+  coins.sort(function (a, b) {
+    return b - a;
+  });
+
+  while (amount > 0) {
+    // prevent browser crashing
+    if (iterations > 1000001) break;
+    iterations++;
+
+    // coin is the highest coin value less than the amount
+    for (var i = 0; i < coins.length; i++ ) {
+      var coin = coins[i];
+
+      if (coin <= amount) {
+        // Add the biggest possible coin to the return array
+        selectedCoins.push(coin);
+
+        // Deduct the coin value from the amount
+        amount = amount - coin;
+        break;
+      }
+    }
+  }
+
+  if (amount > 0) {
+    return [];
+  }
+
+  return selectedCoins.sort(function (a, b) { return a - b})
 }
 
 /**
@@ -22,6 +55,37 @@ function coinChange (amount, coins) {
  */
 
 function scheduleEvents (events) {
+  var selectedEvents = [];
+
+  // sort events with finishing first first
+  events.sort(isFinishFirst);
+
+  // add first event
+  selectedEvents.push(0);
+
+  for (var i = 1; i < events.length; i++) {
+    var previousEvent = selectedEvents[selectedEvents.length - 1];
+    // is there a collision with previous event
+    if (isStartFirst(previousEvent, events[i])) {
+      selectedEvents.push(i);
+    }
+  };
+
+  return selectedEvents;
+}
+
+function isFinishFirst(event1, event2) {
+  var finishHourEarlier = event1.finishHour < event2.finishHour;
+  var finishHourEqual = event1.finishHour === event2.finishHour;
+  var finishMinuteEarlier = event1.finishMinute < event2.finishMinute;
+  return !(finishHourEarlier || (finishHourEqual && finishMinuteEarlier));
+}
+
+function isStartFirst(event1, event2) {
+  var startHourEarlier = event1.startHour < event2.startHour;
+  var startHourEqual = event1.startHour === event2.startHour;
+  var startMinuteEarlier = event1.startMinute < event2.startMinute;
+  return !(startHourEarlier || (startHourEqual && startMinuteEarlier));
 }
 
 /**
@@ -34,5 +98,5 @@ function scheduleEvents (events) {
  * (item-index, percentage). The array should be sorted ascending by item-index.
  */
 
-function naiveRucksack (events) {
+function naiveRucksack (_events) {
 }
