c7b7950abb6daf2a960a0548d9034d4a126c4f41
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-05-17 13:06:28 +0100

    Added the structure for the functions

--- src/algo.js
+++ src/algo.js
@@ -1,13 +1,38 @@
 /**
- * Describe the function to be implememeted.
- * Ideally it be a pure function.
+ * The function will use a Greedy method to solve the problem of giving
+ * change.
  *
- * @param {type} val1 description of the argument
- * @param {type} val2 description of the argument
+ * @param {Integer} the amount for which we have to give change
+ * @param {Array} the values of the coins that are available
  *
- * @return {type} description of the return value
+ * @return {Array} the list of the selected coins, sorted ascending by value
  */
 
-function doSomething (val1, val2) {
-  return;
+function coinChange (amount, coins) {
+}
+
+/**
+ *
+ * @param {Array} the list of show tuples composed of (start_hour, start_minute,
+ * finish_hour, finish_minute)
+ *
+ * @return {Array} array containing the indexes of the selected shows, sorted
+ * ascending by value
+ */
+
+function coinChange (events) {
+}
+
+/**
+ *
+ * @param {Integer} the capacity of the rucksack in kilograms
+ * @param {Array} array of pairs describing the items available to fill the
+ * rucksack. Each pair is formed from weight, followed by value.
+ * finish_hour, finish_minute)
+ *
+ * @return {Array} array of pairs where each pair is composited from
+ * (item-index, percentage). The array should be sorted ascending by item-index.
+ */
+
+function naiveRucksack (events) {
 }
